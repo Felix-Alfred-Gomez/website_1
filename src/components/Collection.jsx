c1c1eed480578cb93684f4ccbf7bf666901121a2
Felix Gomez <felix.alfred.gomez@gmail.com>
2025-04-22 12:06:22 +0200

    Add readme comment

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -5,12 +5,12 @@ const Collection = () => {
   return (
     <div>
         <GuitarImages
-        source="/img/guitare_petite_bouche.jpg" 
+        source="./img/guitare_petite_bouche.jpg" 
         title="Guitare petite bouche" 
         caption="Ceci est une guitare manouche petite bouche parfaite pour les solos"
         />
         <GuitarImages
-        source="/img/guitare_grande_bouche.jpg" 
+        source="./img/guitare_grande_bouche.jpg" 
         title="Guitare grande bouche" 
         caption="Ceci est une guitare manouche petite bouche parfaite pour l'accompagnement"
         />
